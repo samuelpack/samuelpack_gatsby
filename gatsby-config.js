6f0df8a11c2f5fc3e0b7456a727964f7178deb45
samuelpack <packmac2@gmail.com>
2021-08-05 16:39:14 -0400

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "samuelpack_Gatsby",
+    title: "samuelpack.com",
   },
   plugins: [
       "gatsby-plugin-gatsby-cloud",
       "gatsby-plugin-image",
       "gatsby-plugin-sharp",
+      {
+          resolve: "gatsby-source-filesystem",
+          options: {
+              name: 'blog',
+              path: `${__dirname}/blog`,
+          }
+      }
 ],
 };
